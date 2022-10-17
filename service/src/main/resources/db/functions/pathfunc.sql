62fbfd484450641a3d96e664a574ccf69e35e206
Iliad Shaghaghi <ishaghag@verily.com>
2022-10-17 15:50:31 +0000

    Add some comments to the sql function.

--- service/src/main/resources/db/functions/pathfunc.sql
+++ service/src/main/resources/db/functions/pathfunc.sql
@@ -1,5 +1,26 @@
-DROP FUNCTION IF EXISTS pathRecurse;
-CREATE FUNCTION pathRecurse(jsonb, text[], jsonb) RETURNS jsonb as $do$
+-- pathRecurse(target, path, new_value) is a wrapper over jsonb_set which essentially performs:
+
+-- ({"a" : {"b": {"c": null}}}, [a, b, c], "d") ->
+--  jsonb_set({"a": ...}, [a], 
+--      jsonb_set({"b": ...}, [b],
+--          jsonb_set({"c", null}, [c], 
+--              "d")))
+
+-- The jsonb_set spec states:
+-- "Returns target with the item designated by path replaced by new_value, or with new_value added ...
+--  if the item designated by path does not exist. **All earlier steps in the path must exist, 
+--  or the target is returned unchanged.**"
+
+-- Thus, the advantage of splitting the path into individual calls is twofold:
+
+-- a) steps in the path *do not* have to exist -- they will be created with each call.
+-- b) jsonb_set quietly fails if a path element is not indexable (object or array) -- this
+--     function will raise an exception instead.
+-- 
+-- Because of its recursive nature, it must be a named function, so we define it once on startup
+-- rather than inline with the query.
+--
+CREATE OR REPLACE FUNCTION pathRecurse(jsonb, text[], jsonb) RETURNS jsonb as $do$
 DECLARE
     node text;
     child jsonb;
