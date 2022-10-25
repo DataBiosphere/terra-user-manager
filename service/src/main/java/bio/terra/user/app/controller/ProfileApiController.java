edc2789e71a0c603c6729a1bf9172274ea906da6
Iliad Shaghaghi <ishaghag@verily.com>
2022-10-25 15:53:48 +0000

    Allow targeting of users in profile endpoints.

--- service/src/main/java/bio/terra/user/app/controller/ProfileApiController.java
+++ service/src/main/java/bio/terra/user/app/controller/ProfileApiController.java
@@ -3,6 +3,8 @@ package bio.terra.user.app.controller;
 import bio.terra.common.iam.SamUser;
 import bio.terra.user.api.ProfileApi;
 import bio.terra.user.model.AnyObject;
+import bio.terra.user.service.iam.SamRethrow;
+import bio.terra.user.service.iam.SamService;
 import bio.terra.user.service.user.ProfileService;
 import java.util.Arrays;
 import java.util.List;
@@ -19,30 +21,42 @@ public class ProfileApiController implements ProfileApi {
   private final HttpServletRequest servletRequest;
   private final ProfileService profileService;
   private final ProfileSamUserFactory userFactory;
+  private final SamService samService;
 
   @Autowired
   public ProfileApiController(
       HttpServletRequest servletRequest,
       ProfileService profileService,
-      ProfileSamUserFactory userFactory) {
+      ProfileSamUserFactory userFactory,
+      SamService samService) {
     this.servletRequest = servletRequest;
     this.profileService = profileService;
     this.userFactory = userFactory;
+    this.samService = samService;
   }
 
   @Override
-  public ResponseEntity<AnyObject> setUserProfile(AnyObject body, String path) {
-    profileService.setProperty(getUser(), parsePath(path), body.getValue());
+  public ResponseEntity<AnyObject> setUserProfile(AnyObject body, String path, String userEmail) {
+    profileService.setProperty(targetIdOrSelf(userEmail), parsePath(path), body.getValue());
 
-    return getUserProfile(path);
+    return getUserProfile(path, userEmail);
   }
 
   @Override
-  public ResponseEntity<AnyObject> getUserProfile(String path) {
+  public ResponseEntity<AnyObject> getUserProfile(String path, String userEmail) {
     AnyObject apiObj =
-        new AnyObject().value(profileService.getProperty(getUser(), parsePath(path)));
+        new AnyObject()
+            .value(profileService.getProperty(targetIdOrSelf(userEmail), parsePath(path)));
 
     return new ResponseEntity<>(apiObj, HttpStatus.OK);
+  }
+
+  private String targetIdOrSelf(String userEmail) {
+    return StringUtils.isEmpty(userEmail)
+        ? getUser().getSubjectId()
+        : SamRethrow.onInterrupted(
+            () -> samService.adminEmailToId(getUser().getBearerToken(), userEmail),
+            "Admin user Id from email");
   }
 
   private SamUser getUser() {
