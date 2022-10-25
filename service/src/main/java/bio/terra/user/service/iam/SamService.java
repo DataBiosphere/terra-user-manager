b8fe6c73f618da88a8a0b4300de6817281daee85
Iliad Shaghaghi <ishaghag@verily.com>
2022-10-25 15:53:31 +0000

    Add sam service.

---
+++ service/src/main/java/bio/terra/user/service/iam/SamService.java
@@ -0,0 +1,53 @@
+package bio.terra.user.service.iam;
+
+import bio.terra.common.exception.ForbiddenException;
+import bio.terra.common.iam.BearerToken;
+import bio.terra.common.sam.SamRetry;
+import bio.terra.user.app.configuration.SamConfiguration;
+import org.broadinstitute.dsde.workbench.client.sam.ApiClient;
+import org.broadinstitute.dsde.workbench.client.sam.ApiException;
+import org.broadinstitute.dsde.workbench.client.sam.api.AdminApi;
+import org.broadinstitute.dsde.workbench.client.sam.api.UsersApi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SamService {
+  private final SamConfiguration samConfig;
+
+  @Autowired
+  public SamService(SamConfiguration samConfig) {
+    this.samConfig = samConfig;
+  }
+
+  private ApiClient getApiClient(String accessToken) {
+    ApiClient apiClient = getApiClient();
+    apiClient.setAccessToken(accessToken);
+    return apiClient;
+  }
+
+  private ApiClient getApiClient() {
+    return new ApiClient().setBasePath(samConfig.basePath());
+  }
+
+  public UsersApi samUsersApi(String accessToken) {
+    return new UsersApi(getApiClient(accessToken));
+  }
+
+  public AdminApi samAdminApi(String accessToken) {
+    return new AdminApi(getApiClient(accessToken));
+  }
+
+  public String adminEmailToId(BearerToken userRequest, String email) throws InterruptedException {
+    try {
+      return SamRetry.retry(
+          () ->
+              samAdminApi(userRequest.getToken())
+                  .adminGetUserByEmail(email)
+                  .getUserInfo()
+                  .getUserSubjectId());
+    } catch (ApiException apiException) {
+      throw new ForbiddenException("Terra Admin access is required for this action.");
+    }
+  }
+}
